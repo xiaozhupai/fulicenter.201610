7d3c075df2a5684546041804db6ef8acb4dd9995
lzh <414676265@qq.com>
2017-01-16 21:25:00 +0800

    排序箭头的修改

--- app/src/main/java/cn/ucai/fulicenter/controller/fragment/CategoryFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/fragment/CategoryFragment.java
@@ -53,6 +53,7 @@ public class CategoryFragment extends Fragment {
         ButterKnife.bind(this, layout);
         mAdapter=new CategoryAdapter(getContext(),mGroupBean,mChildBean);
         elvCategory.setAdapter(mAdapter);
+        elvCategory.setGroupIndicator(null);
         initView(false);
         initData();
         return layout;
